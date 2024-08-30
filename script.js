121f4700651bdb9d9d065c792a557a6be356039e
priyankraychura <priyankraychura@gmail.com>
2024-08-30 08:17:18 +0530

    Filterable Image Gallery created

---
+++ script.js
@@ -0,0 +1,23 @@
+// Select all buttons and filterable cards
+const filterButtons = document.querySelectorAll('.filter_buttons button');
+const filterableCards = document.querySelectorAll('.filterable_cards .card');
+
+// Define the filter card function
+const filterCards = (e) => {
+    document.querySelector('.active').classList.remove("active");
+    e.target.classList.add('active');
+
+    // Iterate over each filterable card
+    filterableCards.forEach((card) => {
+        // Add hide class to hide the card initally
+        card.classList.add('hide');
+        
+        // Check if the card matches the selected filter or "all" is selected
+        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === 'all'){
+            card.classList.remove('hide');
+        }
+    })
+}
+
+// All click event listner to each button
+filterButtons.forEach(button => button.addEventListener('click', filterCards));
